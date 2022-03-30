434affc501fcb3499c3311eacec7a6e9b403d876
Maycon Carlete <maycon.carlete@gmail.com>
2022-03-29 23:15:01 -0300

    feat: frontend api calling

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,21 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import ErrorPage from './pages/ErrorPage';
+import NavBar from './components/nav-bar';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function App() {
   const { user, isAuthenticated, isLoading} = useAuth0()
   return (
     <Router>
+     <Auth0Provider
+    domain="dev-hqo639s8.us.auth0.com"
+    clientId=""
+    redirectUri={window.location.origin}
+    audience="https://quickstarts/api"
+    scope="read:user"
+  >
+      <NavBar />
       <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
@@ -23,6 +33,7 @@ function App() {
         <Route path='/about' element = {<About />}></Route>
         <Route path='*' element = {<ErrorPage />}></Route>
       </Routes>
+      </Auth0Provider>
     </Router>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,49 @@
-import React from "react"
+import React ,{ useEffect, useState } from "react"
+import { useAuth0 } from "@auth0/auth0-react"
 
 const Home = () => {
-  return <div>Home</div>
+  const { getAccessTokenSilently } = useAuth0();
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = await getAccessTokenSilently({
+          audience: 'https://quickstarts/api',
+          scope: 'read:user',
+        });
+        console.log(token)
+        const response = await fetch('http://localhost:3002/', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        console.log(await response.json())
+        setPosts(await response.json());
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [getAccessTokenSilently]);
+
+  if (!posts) {
+    return <div>Loading...</div>;
+  }
+
+  return <>
+  <div>Home</div>
+    <button onClick={() => getAccessTokenSilently({
+        audience: 'https://quickstarts/api',
+        scope: "read:current_user",
+      }).then(token => fetch('http://localhost:3002', {
+        headers: {
+        Authorization: `Bearer ${token}`
+        }
+      })
+      .then(console.log)
+      .catch(console.log)
+      )}>Click para ver dados</button>
+  </>
 }
 
 export default Home
